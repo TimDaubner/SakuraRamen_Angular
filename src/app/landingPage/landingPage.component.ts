040f2b63f2b113999841f8c3f8ba9fe7de2f6739
studieins <studi.eins@gmx.de>
2025-10-13 14:26:35 +0200

    init Sakura Ramen Website

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,19 @@
+
+import { Component, signal } from '@angular/core';
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [],
+    template: `
+    <section>
+        <h1 class="fontRaleway">SAKURA RAMEN</h1>
+        <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
+    </section>
+    `,
+    styleUrls: ['./landingPage.component.scss']
+})
+export class LandingPageComponent{
+
+}
+
